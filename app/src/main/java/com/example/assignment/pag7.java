690d715b0cc7d90ec2b6ec3a1a3f7db54debe431
imant <i190440@nu.edu.pk>
2022-09-17 21:33:33 +0500

    initial commit

---
+++ app/src/main/java/com/example/assignment/pag7.java
@@ -0,0 +1,14 @@
+package com.example.assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class pag7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pag7);
+    }
+}
